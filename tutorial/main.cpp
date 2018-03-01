7b1c25eb4f7d77d53f771960ffd506b53c79577a
Gao <longgao@8c859046aa00.ant.amazon.com>
2018-02-28 17:52:38 -0800

    Change main file to run conv layers of resnet-18.

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -1,62 +1,191 @@
-/*
-// Copyright (c) 2017 Intel Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-*/
-
-/*! @page tutorial clDNN Tutorial  
-* @section intro Introduction
-*  This section contains 8 chapters of tutorial demonstrating how to work with clDNN. If you are new in clDNN, we recommend to start with
-*  <a href="https://01org.github.io/clDNN/index.html">"clDNN documentation"</a> that describes API. We assume that user is familiar with C++ or C and Deep Learining terminology.
-*  
-* @subpage c1 <br>
-* @subpage c2 <br>
-* @subpage c3 <br>
-* @subpage c4 <br>
-* @subpage c5 <br>
-* @subpage c6 <br>
-* @subpage c7 <br>
-* @subpage c8 <br>
-*
-*/
+#include <../api/CPP/cldnn_defs.h>
 #include <../api/CPP/engine.hpp>
+#include <../api/CPP/input_layout.hpp>
+#include <../api/CPP/memory.hpp>
+#include <../api/CPP/data.hpp>
 #include <../api/CPP/topology.hpp>
+#include <../api/CPP/network.hpp>
+#include <../api/CPP/convolution.hpp>
+#include <iostream>
+#include <string>
+#include <chrono>
 
+#include "helper_functions.h"
 
-cldnn::engine   chapter_1();                                    // Engine, layout, tensor, memory, data and input
-cldnn::topology chapter_2(cldnn::engine&);                      // Primitives and topology
-void            chapter_3(cldnn::engine&, cldnn::topology&);    // Network and execution
-void            chapter_4(cldnn::engine&, cldnn::topology&);    // Hidden layers access
-void            chapter_5(cldnn::engine&, cldnn::topology&);    // Other building options
-void            chapter_6(cldnn::engine&);                      // How to add a kernel to clDNN
-void            chapter_7(cldnn::engine&);                      // How to create a custom primitive (without changing clDNN)
-void            chapter_8(cldnn::engine&);                      // Extended profiling for networks built with optimized data
+double run_conv(tensor& input_tensor, tensor& weights_tensor, tensor& biases_tensor, tensor& stride_tensor, tensor& padding_tensor, std::string data_type);
+tensor create_tensor(int batch, int feature, int x, int y);
 
 int main()
 {
-    try {
-        cldnn::engine eng = chapter_1();
-        cldnn::topology topology = chapter_2(eng);
-        chapter_3(eng, topology);
-        chapter_4(eng, topology);
-        chapter_5(eng, topology);
-        chapter_6(eng);
-        chapter_7(eng);
-        chapter_8(eng);
-    }
-    catch (const std::exception& ex)
+    int iter = 10;
+    double exe_time = 10;
+    std::string data_type = "float32"; // "float32" or "float16"
+    int batch = 1, in_size = 224, channel = 3, filter = 64, kernel = 7, padding = 3, stride = 2;
+
+    std::cout << "conv layer (1, 224, 3, 64, 7, 3, 2)"<< std::endl;
+    batch = 1, in_size = 224, channel = 3, filter = 64, kernel = 7, padding = 3, stride = 2;
+    tensor input_tensor = create_tensor(batch, channel, in_size, in_size);
+    tensor weights_tensor = create_tensor(filter, channel, kernel, kernel);
+    tensor biases_tensor = create_tensor(1, 1, filter, 1);
+    tensor stride_tensor = create_tensor(1, 1, stride, stride);
+    tensor padding_tensor = create_tensor(0, 0, -padding, -padding);
+    exe_time = 0;
+    for (int i = 0; i < iter; i++)
     {
-        std::cout << ex.what();
+        exe_time += run_conv(input_tensor, weights_tensor, biases_tensor, stride_tensor, padding_tensor, data_type);
     }
+    std::cout << "Executing time: " << (exe_time / iter) << " milliseconds"<< std::endl;
+
+    std::cout << "conv layer (1, 56, 64, 64, 3, 1, 1)"<< std::endl;
+    batch = 1, in_size = 56, channel = 64, filter = 64, kernel = 3, padding = 1, stride = 1;
+    input_tensor = create_tensor(batch, channel, in_size, in_size);
+    weights_tensor = create_tensor(filter, channel, kernel, kernel);
+    biases_tensor = create_tensor(1, 1, filter, 1);
+    stride_tensor = create_tensor(1, 1, stride, stride);
+    padding_tensor = create_tensor(0, 0, -padding, -padding);
+    exe_time = 0;
+    for (int i = 0; i < iter; i++)
+    {
+        exe_time += run_conv(input_tensor, weights_tensor, biases_tensor, stride_tensor, padding_tensor, data_type);
+    }
+    std::cout << "Executing time: " << (exe_time / iter) << " milliseconds"<< std::endl;
+
+    std::cout << "conv layer (1, 28, 128, 128, 3, 1, 1)"<< std::endl;
+    batch = 1, in_size = 28, channel = 128, filter = 128, kernel = 3, padding = 1, stride = 1;
+    input_tensor = create_tensor(batch, channel, in_size, in_size);
+    weights_tensor = create_tensor(filter, channel, kernel, kernel);
+    biases_tensor = create_tensor(1, 1, filter, 1);
+    stride_tensor = create_tensor(1, 1, stride, stride);
+    padding_tensor = create_tensor(0, 0, -padding, -padding);
+    exe_time = 0;
+    for (int i = 0; i < iter; i++)
+    {
+        exe_time += run_conv(input_tensor, weights_tensor, biases_tensor, stride_tensor, padding_tensor, data_type);
+    }
+    std::cout << "Executing time: " << (exe_time / iter) << " milliseconds"<< std::endl;
+    
+    std::cout << "conv layer (1, 14, 256, 256, 3, 1, 1)"<< std::endl;
+    batch = 1, in_size = 14, channel = 256, filter = 256, kernel = 3, padding = 1, stride = 1;
+    input_tensor = create_tensor(batch, channel, in_size, in_size);
+    weights_tensor = create_tensor(filter, channel, kernel, kernel);
+    biases_tensor = create_tensor(1, 1, filter, 1);
+    stride_tensor = create_tensor(1, 1, stride, stride);
+    padding_tensor = create_tensor(0, 0, -padding, -padding);
+    exe_time = 0;
+    for (int i = 0; i < iter; i++)
+    {
+        exe_time += run_conv(input_tensor, weights_tensor, biases_tensor, stride_tensor, padding_tensor, data_type);
+    }
+    std::cout << "Executing time: " << (exe_time / iter) << " milliseconds"<< std::endl;
+
+    std::cout << "conv layer (1, 7, 512, 512, 3, 1, 1)"<< std::endl;
+    batch = 1, in_size = 7, channel = 512, filter = 512, kernel = 3, padding = 1, stride = 1;
+    input_tensor = create_tensor(batch, channel, in_size, in_size);
+    weights_tensor = create_tensor(filter, channel, kernel, kernel);
+    biases_tensor = create_tensor(1, 1, filter, 1);
+    stride_tensor = create_tensor(1, 1, stride, stride);
+    padding_tensor = create_tensor(0, 0, -padding, -padding);
+    exe_time = 0;
+    for (int i = 0; i < iter; i++)
+    {
+        exe_time += run_conv(input_tensor, weights_tensor, biases_tensor, stride_tensor, padding_tensor, data_type);
+    }
+    std::cout << "Executing time: " << (exe_time / iter) << " milliseconds"<< std::endl;
+
     return 0;
+
+}
+
+tensor create_tensor(int batch, int feature, int x, int y) {
+    tensor ret(batch, feature, x, y);
+    return ret;
+}
+double run_conv(tensor& input_tensor, tensor& weights_tensor, tensor& biases_tensor, tensor& stride_tensor, tensor& padding_tensor, std::string data_type)
+{
+    //std::cout << "Run conv2d using clDNN." << std::endl;
+
+    // Create an engine
+    const bool profiling = true;
+    engine engine(profiling);
+
+    // Create input memory for convolution layer
+    memory input_prim = memory::allocate(engine, { data_types::f16, format::bfyx, input_tensor });
+    memory weights    = memory::allocate(engine, { data_types::f16, format::bfyx, weights_tensor });
+    memory biases     = memory::allocate(engine, { data_types::f16, format::bfyx, biases_tensor });
+    if (data_type == "float32")
+    {
+        input_prim = memory::allocate(engine, { data_types::f32, format::bfyx, input_tensor });
+        weights    = memory::allocate(engine, { data_types::f32, format::bfyx, weights_tensor });
+        biases     = memory::allocate(engine, { data_types::f32, format::bfyx, biases_tensor });
+    }
+    //std::cout << "memory allocation done." << std::endl;
+    set_values(input_prim, get_simple_data<float>(input_prim));
+    set_values(weights,    get_simple_data<float>(weights));
+    set_values(biases,     get_simple_data<float>(biases));
+
+    // Create a topology with a simple Convolution layer
+    topology topology(
+        input_layout("conv_input", input_prim.get_layout()),
+        data("conv_weights", weights),
+        data("conv_biases", biases),
+        convolution(
+            "conv",
+            "conv_input",
+            { "conv_weights" },
+            { "conv_biases" },
+            stride_tensor,
+            padding_tensor,
+            { 1, 1, 1, 1 },
+            false,
+            0,
+            padding{ { 0, 0, 0, 0 }, 0 })
+    );
+
+    build_options build_opt;
+    // Optimize_data flag can change weights and outputs layouts. Let take a look at 
+    // Set option to optimize data.
+    build_opt.set_option(build_option::optimize_data(true));
+
+
+    network network(engine, topology, build_opt);
+
+    //auto start = std::chrono::steady_clock::now();
+
+    // Set input.
+    network.set_input_data("conv_input", input_prim);
+    // Ready to go.
+    auto outputs = network.execute();
+    //auto elapsed =  std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start);
+    //std::cout << "Elapsed time: " << elapsed.count() << std::endl;
+    // Get primitives that were executed and their events needed for profiling
+    auto executed_primitives = network.get_executed_primitives();
+
+    // Now, we want to check what is the time of execution of each primitive:
+    std::vector<cldnn::instrumentation::profiling_info> profiling_table;
+    for (auto& p : executed_primitives)
+    {
+        profiling_table.push_back({ p.first, p.second.get_profiling_info() });
+    }
+
+    // We have table of profiling metrics.
+    for (auto& p : profiling_table)
+    {
+        if (p.name == "conv")
+        {
+            //std::cout << p.name << ":" << std::endl;
+            double executing_time = 0;
+            for (auto& q : p.intervals)
+            {
+                //std::cout << "\t" << q.name << ": " << std::chrono::duration_cast<std::chrono::duration<double, std::chrono::milliseconds::period>>(q.value->value()).count()
+                    //<< " milliseconds" << std::endl;
+                if (q.name == "executing")
+                {
+                    executing_time = q.value->value().count() / 1000000.0;
+                }
+            }
+            //std::cout << "\t" << "conv executing time: " << executing_time << " milliseconds"<< std::endl;
+            return executing_time;
+        }
+    }
+    return -1000;    
 }
